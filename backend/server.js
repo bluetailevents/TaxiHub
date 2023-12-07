db6440ce8567ee0aa1ec73397a47fef7be9bfb4c
Compphsy <gavinblue30@gmail.com>
2023-12-07 16:04:26 +0000

    21

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,6 @@ app.use('/api/geojson/', require('./routes/geojsonRoutes'));
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
-a
   app.get('*', (req, res) =>
     res.sendFile(
       path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
