894e29d0bb36ee49f28b4bb04c635cdb7cd7ed65
Compphsy <gavinblue30@gmail.com>
2023-12-09 08:44:49 +0000

    update sever.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const express = require('express');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
